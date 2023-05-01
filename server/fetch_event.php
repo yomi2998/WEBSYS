e466cd6a96dc870972ff4443ec6b954f41444574
yomi2998 <130840857+yomi2998@users.noreply.github.com>
2023-05-01 15:43:17 +0800

    glhf

--- server/fetch_event.php
+++ server/fetch_event.php
@@ -6,11 +6,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $myphpadmin_dbname = "fun_fair";
     $conn = new mysqli($myphpadmin_host, $myphpadmin_username, $myphpadmin_password, $myphpadmin_dbname);
 
-    if (!$conn->connect_error) {
-    $stmt = $conn->prepare("SELECT * FROM events ORDER BY event_date_time ASC");
-    $stmt->execute();
-    $result1 = $stmt->get_result();
-    // not finished!!!!!!!!!!!!!!!!!
+    $sql = "SELECT * FROM events WHERE event_date_time <= DATE_ADD(NOW(), INTERVAL 14 DAY) ORDER BY event_date_time ASC";
+    $result = $conn->query($sql);
+
+    $rows = array();
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
     }
+    $conn->close();
+
+    $json = json_encode($rows);
+    header('Content-Type: application/json');
+    echo $json;
 }
 ?>
